c7fcf7c82d63c5a718571d9eeef17ea0d7efc682
rachid elaid <rachidelaid@gmail.com>
2022-03-31 14:21:49 +0000

    add each method in class and filter in module

--- enumerables.rb
+++ enumerables.rb
@@ -5,4 +5,17 @@ module MyEnumerable
     end
     return false
   end
+
+  def all?
+    @list.each do |element|
+      return false if yield element
+    end
+    return true
+  end
+
+  def filter
+    res = []
+    each { |n| res.push(n) }
+    res
+  end
 end
--- mylist.rb
+++ mylist.rb
@@ -1,5 +1,35 @@
+require_relative './enumerables.rb'
+
 class MyList
+  include MyEnumerable
+
   def initialize(*list)
     @list = list
   end
+
+  def each
+    for n in @list do
+      yield n
+    end
+  end
 end
+
+# Create our list
+list = MyList.new(1, 2, 3, 4)
+#=> #<MyList: @list=[1, 2, 3, 4]>
+
+# Test #all?
+puts list.all? {|e| e < 5}
+#=> true
+puts list.all? {|e| e > 5}
+#=> false
+
+# Test #any?
+puts list.any? {|e| e == 2}
+#=> true
+puts list.any? {|e| e == 5}
+#=> false
+
+# Test #filter
+print list.filter {|e| e.even?}
+#=> [2, 4]
